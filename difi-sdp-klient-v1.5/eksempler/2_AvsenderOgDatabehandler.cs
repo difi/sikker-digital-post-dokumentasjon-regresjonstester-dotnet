d214012948308846fd1bf0c674cc46c403fbb7a0
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-02-02 17:47:29 +0100

    Lagt til v1.5 og gjort alle til console-application

---
+++ difi-sdp-klient-v1.5/eksempler/2_AvsenderOgDatabehandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Aktører;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Post;
+
+namespace difi_sdp_klient_v1._2.eksempler
+{
+    public class AvsenderOgDatabehandler
+    {
+        public void Initier()
+        {
+            var orgnummerAvsender = new Organisasjonsnummer("123456789");
+            var avsender = new Avsender(orgnummerAvsender);
+
+            var orgnummerDatabehandler = new Organisasjonsnummer("987654321");
+            var avsendersertifikat = new X509Certificate2();
+            var databehandler = new Databehandler(orgnummerDatabehandler, avsendersertifikat);
+
+            avsender.Avsenderidentifikator = "Avsenderidentifikator I Organisasjon";
+        }
+    }
+}
---
+++ difi-sdp-klient-v1.5/eksempler/3_Forsendelse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Aktører;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Post;
+
+namespace difi_sdp_klient_v1._2.eksempler
+{
+    public class LagForsendelse
+    {
+        public void OppretteForsendelse()
+        {
+            var hoveddokument = new Dokument(
+                    tittel: "Dokumenttittel", 
+                    dokumentsti: "/Dokumenter/Hoveddokument.pdf", 
+                    mimeType: "application/pdf", 
+                    språkkode: "NO", 
+                    filnavn: "filnavn"
+                );
+
+            var dokumentpakke = new Dokumentpakke(hoveddokument);
+
+            var vedleggssti = "/Dokumenter/Vedlegg.pdf";
+            var vedlegg = new Dokument(
+                tittel: "tittel", 
+                dokumentsti: vedleggssti, 
+                mimeType: "application/pdf", 
+                språkkode: "NO", 
+                filnavn: "filnavn");
+
+            dokumentpakke.LeggTilVedlegg(vedlegg);
+
+            Avsender avsender = null; //Som initiert tidligere
+            PostInfo postInfo = null; //Som initiert tidligere
+            var forsendelse = new Forsendelse(avsender,postInfo,dokumentpakke);
+        }
+    }
+}
